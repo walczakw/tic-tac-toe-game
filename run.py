10029c9a7ed38e86a02da78966d4057b508122c6
walczakw <walczakw864@gmail.com>
2023-03-12 00:14:44 +0000

    Add comments to outline plan for a game

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# 1. Start
+# 2. Display instructions and ask to press Enter to start (validate)
+# 3. Ask for player's name (validate)
+# 4. Ask to pick a symbol (X or O) (validate)
+# 5. Create board (python list)
+# 6. Display board
+# 7. Player starts -> Ask player to make a move (validate)
+# 8. Display board after player's move
+# 9. Computer randomly select it's move
+# 10. Display board after player's move + computer's move
+# 11. Check for winner - if there's 3 in a row in any of:
+# check rows
+# check columns
+# check diagonal
+# 12. Check for tie - if the board is full
+# 13. If there is no winner/tie -> repeat 7 - 12
+# 14. If there is a winner/tie -> print message
+# 15. Ask the player to play again (Y/N) (validate)
+# No -> end game
+# Yes -> Reset and flip players (Computer starts)
+
