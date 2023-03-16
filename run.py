2fe3338ba8bae25d37e1dc4ad92eae963bb7e290
walczakw <walczakw864@gmail.com>
2023-03-16 01:23:47 +0000

    Add function for displaying board

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 # 1. Start
-# 2. Display instructions and ask to press Enter to start (validate)
+# 2. Display instructions and ask to press any key to start (validate)
 # 3. Ask for player's name (validate)
-# 4. Ask to pick a symbol (X or O) (validate)
+# 4. Ask to pick a symbol (X or O) (validate) save as global variable
 # 5. Create board (python list)
 # 6. Display board
-# 7. Player starts -> Ask player to make a move (validate)
+# 7. Player starts -> Ask player to make a move (validate) check numbers, is already taken (is not dash inside)
 # 8. Display board after player's move
-# 9. Computer randomly select it's move
+# 9. Computer randomly select it's move (function check position)
 # 10. Display board after player's move + computer's move
-# 11. Check for winner - if there's 3 in a row in any of:
+# 11. Check for winner - if there's 3 in a row in any of: (check after every input)
 # check rows
 # check columns
 # check diagonal
@@ -17,5 +17,23 @@
 # 14. If there is a winner/tie -> print message
 # 15. Ask the player to play again (Y/N) (validate)
 # No -> end game
-# Yes -> Reset and flip players (Computer starts)
+# Yes -> Reset and flip players (Computer starts) (make sure it starts with clear board)
 
+
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+
+# def main():
+
+# Function for displaying board
+def display_board():
+    print(board[0] + " | " + board[1] + " | " + board[2] + "     1 | 2 | 3")
+    print(board[3] + " | " + board[4] + " | " + board[5] + "     4 | 5 | 6")
+    print(board[6] + " | " + board[7] + " | " + board[8] + "     7 | 8 | 9")
+
+
+print(display_board())
+
+
+# main()
