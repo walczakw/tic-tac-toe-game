25a140e0ac3027902d0bf172067beeeebc2dd209
walczakw <walczakw864@gmail.com>
2023-03-17 02:32:27 +0000

    Add game instructions

--- run.py
+++ run.py
@@ -24,18 +24,46 @@ board = [" ", " ", " ",
          " ", " ", " ",
          " ", " ", " "]
 
-# def main():
 
 # Function for displaying board
 def display_board():
-    print(board[0] + "     | " + board[1] + " | " + board[2] + "       1 | 2 | 3")
-    print("    --+---+--       --+---+--")
-    print(board[3] + "     | " + board[4] + " | " + board[5] + "       4 | 5 | 6")
-    print("    --+---+--       --+---+--")
-    print(board[6] + "     | " + board[7] + " | " + board[8] + "       7 | 8 | 9")
+    print("")
+    print(board[0] + "   | " + board[1] + " | " + board[2] + "      1 | 2 | 3")
+    print("  --+---+--      --+---+--")
+    print(board[3] + "   | " + board[4] + " | " + board[5] + "      4 | 5 | 6")
+    print("  --+---+--      --+---+--")
+    print(board[6] + "   | " + board[7] + " | " + board[8] + "      7 | 8 | 9")
+    print("")
 
 
 print(display_board())
+
+
+# 1. Start
+
+# def main():
+
+# 2. Display instructions and ask to press any key to start (validate)
+print("")
+print("")
+print("              WELCOME TO TIC-TAC-TOE GAME !!!")
+print("")
+print("")
+print("How to play:")
+print("1. The game is played on a board that's 3 squares by 3 squares")
+print("2. You are X, the computer is O. Players take turns putting their marks in empty squares")
+print("3. You put a mark by pressing a number on a keyboard that corresponds with empty square (see board below)")
+print("4. The first player to get 3 of their marks in a row (up, down, across, or diagonally) is the winner")
+print("5. When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie")
+print("")
+print(display_board())
+print("")
+print("Press any key to start...")
+print("")
+
+# 3. Ask for player's name (validate)
+print("What's your name?")
+print("")
 
 
 # main()
